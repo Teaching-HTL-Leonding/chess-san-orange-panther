2848cfacd1985c68a99ffba39a1247e6607be0ac
Kathi Einzenberger <113719929+orange-panther@users.noreply.github.com>
2023-04-20 10:30:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿var moves = args;
+for (int i = 0; i < moves.Length; i++)
+{
+    DecipherMove(moves[i], out string piece, out string position);
+    System.Console.WriteLine($"{piece} moves to {position}");
+}
+
+void DecipherMove(string move, out string piece, out string position)
+{
+    if (move.Length == 2)
+    {
+        piece = "Pawn (♙ )";
+        position = move;
+    }
+    else 
+    {
+        position = move.Substring(move.Length -2);
+        piece = move.Substring(0, 1) switch {
+            "B" => "Bishop (♗ )",
+            "N" => "Knight (♘ )",
+            "Q" => "Queen (♕ )",
+            "R" => "Rook (♖ )",
+            "K" => "King (♔ )",
+            _ => ""
+        };
+    }
+}
